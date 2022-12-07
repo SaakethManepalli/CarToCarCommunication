b9e97fac93f71e1669031ffcb5e2cd8670f418e7
Saaketh N <46768286+datboisaaketh@users.noreply.github.com>
2022-12-06 21:34:43 -0500

    12/6/22 V2

--- SERVER/server.py
+++ SERVER/server.py
@@ -1,21 +1,55 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import cgi
 
-tasklist = ["Task One", "Task Two", "Take Three"]
+DATA = ["Task One", "Task Two", "Take Three"]
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("content-type", "text/html")
-        self.end_headers()
-        
-        output = ''
-        output += '<html><body>'
-        output += '<h1>Task List</h1>'
-        for task in tasklist:
-            output += task
-            output += '</br>'
-        output += '</body></html>'
-        self.wfile.write(output.encode())
+        if self.path.endswith('/tasklist'):
+            self.send_response(200)
+            self.send_header("content-type", "text/html")
+            self.end_headers()
+            
+            output = ''
+            output += '<html><body>'
+            output += '<h1>Task List</h1>'
+            output += '<h3><a href="/tasklist/new">Add New Task</a></h3>'
+            for task in DATA:
+                output += task
+                output += '</br>'
+            output += '</body></html>'
+            self.wfile.write(output.encode())
+
+        if self.path.endswith('/new'):
+            self.send_response(200)
+            self.send_header('content-type', 'text/html')
+            self.end_headers()
+
+            output =''
+            output += '<html><body>'
+            output += '<h1> Add New Data </h1>'
+
+            output += '<form method="POST" enctype="multipart/form-data" action="/tasklist/new">'
+            output += '<input name="task" type="text" placeholder="Add new task">'
+            output += '<input type ="submit" value="Add">'
+            output += '</form>'
+            output += '</body></html>'
+
+            self.wfile.write(output.encode())
+
+
+    def do_POST(self):
+        if self.path.endswith('/new'):
+            ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                new_task = fields.get('task')
+                DATA.append(new_task) # Could Add Time Element Here
+
+            self.send_response(301)
+            self.send_header('content-type', 'text/html')
+            self.send_header('Location', '/tasklist')
+            self.end_headers()
 
 
 
