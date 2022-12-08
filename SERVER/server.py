4359fd43f9d06ca1599323d357bcf0ecbba8e8e2
Saaketh M <46768286+datboisaaketh@users.noreply.github.com>
2022-12-08 12:33:45 -0500

    12/8/33

--- SERVER/server.py
+++ SERVER/server.py
@@ -5,7 +5,7 @@ DATA = ["Task One", "Task Two", "Take Three"]
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path.endswith('/tasklist'):
+        if self.path.endswith('/DATA'):
             self.send_response(200)
             self.send_header("content-type", "text/html")
             self.end_headers()
@@ -16,6 +16,7 @@ class RequestHandler(BaseHTTPRequestHandler):
             output += '<h3><a href="/tasklist/new">Add New Task</a></h3>'
             for task in DATA:
                 output += task
+                output += "<a/ href='/DATA/%s/remove'>X</a>"
                 output += '</br>'
             output += '</body></html>'
             self.wfile.write(output.encode())
@@ -29,7 +30,7 @@ class RequestHandler(BaseHTTPRequestHandler):
             output += '<html><body>'
             output += '<h1> Add New Data </h1>'
 
-            output += '<form method="POST" enctype="multipart/form-data" action="/tasklist/new">'
+            output += '<form method="POST" enctype="multipart/form-data" action="/DATA/new">'
             output += '<input name="task" type="text" placeholder="Add new task">'
             output += '<input type ="submit" value="Add">'
             output += '</form>'
@@ -37,14 +38,33 @@ class RequestHandler(BaseHTTPRequestHandler):
 
             self.wfile.write(output.encode())
 
+        if self.path.endswith('/remove'):
+            listIDPath = self.path.split('/')[2]
+            print(listIDPath)
+            self.send_response(200)
+            self.send_header('content-type', 'text/html')
+            self.end_headers()
+
+            output =''
+            output +='<html><body>'
+            output += '<h1>Remove task: %s</h1>' % listIDPath.replace('%20', '')
+            output +='<h1>Remove task: %s </h1>' % listIDPath.replace('%20', '')
+            
+            output += '</body></html>'
+            self.wfile.write(output.encode())
 
     def do_POST(self):
         if self.path.endswith('/new'):
             ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+            pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+            content_len = int(self.headers.get('Content-length'))
+            pdict['CONTENT-LENGTH'] = content_len
+
             if ctype == 'multipart/form-data':
                 fields = cgi.parse_multipart(self.rfile, pdict)
                 new_task = fields.get('task')
-                DATA.append(new_task) # Could Add Time Element Here
+                DATA.append(new_task[0])
+                
 
             self.send_response(301)
             self.send_header('content-type', 'text/html')
@@ -60,6 +80,10 @@ def main():
     server = HTTPServer(("", PORT), RequestHandler)
     print("Server Running on %s" % PORT)
     server.serve_forever()
+
+if KeyboardInterrupt:
+    server = HTTPServer(("", 8000), RequestHandler)
+    server.server_close()
 
 
 if __name__ == "__main__":
