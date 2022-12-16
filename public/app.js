7000bc128460714cbead75c6fce53d54b8e055d6
Saaketh M <46768286+datboisaaketh@users.noreply.github.com>
2022-12-16 12:34:48 -0500

    12/16

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+// Google Auth
 const auth = firebase.auth();
 
 const whenSignedIn = document.getElementById('whenSignedIn');
@@ -9,3 +10,51 @@ const signOutBtn = document.getElementById('signOutBtn');
 const userDetails = document.getElementById('userDetails');
 
 const provider = new firebase.auth.GoogleAuthProvider();
+
+// Firestore
+
+const db = firebase.firestore();
+
+const addData = document.getElementById('addData')
+const GPS = document.getElementById('GPSSET')
+
+let DATAref;
+let unsubscribe;
+
+    if (user) {
+        DATAref = db.collection('Data')
+
+        addData.onclick =() => {
+            const {serverTimestamp} = firebase.firestore.fieldvalue;
+
+            DATAref.add({
+                uid: user.uid,
+                name: faker.commerce.productName();
+                createdAt: serverTimestamp()
+            });
+
+            
+        }
+
+        unsubscribe = DATAref
+            .where('uid', '==', user.uid)
+            .orderBy('createdAt')
+            .onSnapshot(querySnapshot) => {
+
+                const items = querySnapshot.docs.map(doc =>) {
+
+                    return `<li${doc.data().name}</li>`
+                });
+            }
+
+
+
+
+
+
+
+
+
+
+
+    }
