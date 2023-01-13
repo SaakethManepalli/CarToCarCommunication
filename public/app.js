a16d13e29d04b5d1bfaf7956f4d91ed02fc829bd
Saaketh M <46768286+datboisaaketh@users.noreply.github.com>
2023-01-13 12:11:09 -0500

    god help hannah ren
    shes skipping besdies that look at this

--- public/app.js
+++ public/app.js
@@ -1,4 +1,3 @@
-// Google Auth
 const auth = firebase.auth();
 
 const whenSignedIn = document.getElementById('whenSignedIn');
@@ -9,52 +8,83 @@ const signOutBtn = document.getElementById('signOutBtn');
 
 const userDetails = document.getElementById('userDetails');
 
+
 const provider = new firebase.auth.GoogleAuthProvider();
 
-// Firestore
+/// Sign in event handlers
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
+
+
+
+///// Firestore /////
 
 const db = firebase.firestore();
 
-const addData = document.getElementById('addData')
-const GPS = document.getElementById('GPSSET')
+const createThing = document.getElementById('createThing');
+const thingsList = document.getElementById('thingsList');
 
-let DATAref;
+
+let thingsRef;
 let unsubscribe;
 
+auth.onAuthStateChanged(user => {
+
     if (user) {
-        DATAref = db.collection('Data')
 
-        addData.onclick =() => {
-            const {serverTimestamp} = firebase.firestore.fieldvalue;
+        // Database Reference
+        thingsRef = db.collection('things')
 
-            DATAref.add({
+        createThing.onclick = () => {
+
+            const { serverTimestamp } = firebase.firestore.FieldValue;
+
+            thingsRef.add({
                 uid: user.uid,
-                name: faker.commerce.productName();
+                name: faker.commerce.productName(),
                 createdAt: serverTimestamp()
             });
-
-            
         }
 
-        unsubscribe = DATAref
+
+        // Query
+        unsubscribe = thingsRef
             .where('uid', '==', user.uid)
-            .orderBy('createdAt')
-            .onSnapshot(querySnapshot) => {
+            .orderBy('createdAt') // Requires a query
+            .onSnapshot(querySnapshot => {
+                
+                // Map results to an array of li elements
 
-                const items = querySnapshot.docs.map(doc =>) {
+                const items = querySnapshot.docs.map(doc => {
 
-                    return `<li${doc.data().name}</li>`
+                    return `<li>${doc.data().name}</li>`
+
                 });
-            }
+
+                thingsList.innerHTML = items.join('');
+
+            });
 
 
 
-
-
-
-
-
-
-
-
+    } else {
+        // Unsubscribe when the user signs out
+        unsubscribe && unsubscribe();
     }
+});
